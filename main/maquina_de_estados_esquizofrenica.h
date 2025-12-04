e3c8594c4d2219980a892cdeeb2c47c51181f211
MaartaG <cgmarta12@gmail.com>
2025-12-04 10:16:15 +0100

    Concretando valores iniciales y estructura
    Más o menos he establecido los nombres para las fases que creo correctos, pero aún hay varias cosas que quiero revisar y falta por acabar la estructura base.

--- main/maquina_de_estados_esquizofrenica.h
+++ main/maquina_de_estados_esquizofrenica.h
@@ -15,8 +15,8 @@
  *      - @ref APERTURA 3 seg
  * 
  * - @ref ESTADO_SEGURIDAD:
+ *      - @ref apertura de la mano   
  *      - @ref parada de motores
- *      - @ref apertura de la mano 
  *      - @ref cambio a @ref estado_normal
  * 
  * - @ref ESTADO_CALIBRADO_UMBRALES:
@@ -37,7 +37,187 @@
 /** Determinación nuevos criterios para las fases:
  * 
  * FASE_INICIO << en esta se realiza el incio de los cuatro estados excepto el de seguridad
- * FASE_FINAL << en esta se realiza el último paso de los estados
+ * FASE_PASO_1 << en esta se realiza el primer paso, es decir, la primera acción de dicho estado
+ * FASE_PASO_2 << en esta se realiza el segundo paso, es decir, la acción siguiente a la FASE_PASO 1
  * FASE_CAMBIO_ESTADO << en esta se realiza el cambio de estados tras cierta función/configuración/hecho.
- * FASE_
  */
+
+#pragma once
+
+/**
+ * @enum Estado_Protesis
+ * @brief Posibles estados de la prótesis
+ */
+enum Estado_Protesis 
+{
+    ESTADO_NORMAL,             ///< Funcionamiento normal de la prótesis.
+    ESTADO_DESCANSO,           ///< Modo para realizar una pausa controlada.
+    ESTADO_SEGURIDAD,          ///< Modo de seguridad (secuencia: apertura y parada de motores)
+    ESTADO_CALIBRADO_UMBRALES, ///< Modo de calibración de umbrales de activación/desactivación.
+    ESTADO_CALIBRADO_MOTORES   ///< Modo de calibración de límites mecánicos del motor.
+};
+
+/**
+ * @enum Fase_Estado
+ * @brief Fases asociadas a cada estado de la prótesis.
+ */
+enum Fase_Estado{};
+
+/**
+ * @enum Causa_Seguridad
+ * @brief Enumera las causas/condiciones por las cuales entraríamos en @ref ESTADO_SEGURIDAD.
+ */
+enum Causa_Seguridad 
+{
+    causas_ninguna = 0, 
+    causa_sobrecorriente,     ///< sobrecorriente en motores.
+    causa_sobrecalentameinto, ///<  niveles de temperatura demasiado elevados.
+    causa_velocidad_excesiva, ///<  velocidad fuera de límites (ESTA NO SÉ AÚN SI SE QUEDARÁ O NO)
+    causa_fuerza_excesiva,    ///< fuerza fuera de límites (ESTA NO SÉ AÚN SI SE QUEDARÁ O NO)
+    causa_perdida_sensores,   ///< ha habido una perdida de señal de los sensores
+    causa_error_encoder       ///< fallo en la lectura de la posición
+};
+
+/**
+ * @struct Condiciones_Seguridad
+ * @brief Estructura donde establecemos los parámetros a medir y sus valores máximos. Además, tenemos el monitoreo de dichos parámetros.
+ */
+struct Condiciones_Seguridad 
+{
+    float corriente_motores; ///< comprueba el valor de la corriente de los motores.
+    float temperatura;       ///< comprueba los niveles de temperatura.
+    float velocidad;         ///< comprueba el valor de la velocidad.
+    float fuerza;            ///< comprueba el valor de la fuerza.
+    float señal_sensores;    ///< ESTA VARIABLE TENDRÁ QUE SER UNA GLOBAL PARA OBTENER LOS VALORES DE LA SEÑAL DKT QUE SE OBTIENE EN MOTORES.
+    float posicion;          ///< ESTA VARIABLE OBTIENE LAS VARIABLES DE MOTORES DE LAS FUNCIONES ESAS DE POSICIÓN (¿SÍ O NO?)
+
+    // Estos son los valores máximos que tenemos que establecer (los umbrales): 
+    const float UMBRAL_CORRIENTE = ;
+    const float UMBRAL_TEMPERATURA = ;
+    const float UMBRAL_VELOCIDAD = ;
+    const float UMBRAL_FUERZA = ;
+
+    /**
+     * @brief Verifica si algún parámetro está en nivel crítico
+     * @return Código de causa si hay problema, 0 si todo correcto.
+     */
+    Verificar_Parametros () // BUSCAR QUE TIPO DE FUNCIÓN HAY QUE PONER EN ESTE CASO
+    {
+        if(corriente_motores > UMBRAL_CORRIENTE) 
+        {
+            return causa_sobrecorriente;
+        };
+
+        if(temperatura > UMBRAL_TEMPERATURA) 
+        {
+            return causa_sobrecalentameinto;
+        };
+
+        if(velocidad > UMBRAL_VELOCIDAD) 
+        {
+            return causa_velocidad_excesiva;
+        };
+
+        if(fuerza > UMBRAL_FUERZA) 
+        {
+            return causa_fuerza_excesiva;
+        };
+
+        if (señal_sensores == 0) 
+        {
+            return causa_perdida_sensores;
+        };
+
+        if (posicion == 0)
+        {
+            return causa_error_encoder;
+        };
+        
+        return causas_ninguna;
+
+    };
+
+    /**
+    * @brief en esta vamos actualizando los valores del inicio.
+    */
+    void actualizacion (float corriente_actual, float temp_actual, float velocidad_actual, float fuerza_actual, float señal_actual, float posicion_actual) 
+    {
+        corriente_actual = corriente_motores; 
+        temp_actual = temperatura;      
+        velocidad_actual = velocidad;        
+        fuerza_actual = fuerza;            
+        señal_actual = señal_sensores;    
+        posicion_actual = posicion;
+    };
+
+};
+
+
+/**
+  * @struct Maquina_de_estados_protesis
+  * @brief Estructura que almacena y gestiona el estado y fase actual de la prótesis.
+  */
+struct Maquina_de_estados_protesis
+{
+    enum Estado_Protesis estado_actual; ///< Estado actual de la prótesis.
+    enum Fase_Estado fase_actual;       ///< Fase actual dentro del estado.
+    enum Causa_Seguridad error_actual;  ///< Error que provoca que haya que entrar en @ref ESTADO_SEGURIDAD
+
+    /**
+      * @brief Constructor que inicializa la prótesis en @ref ESTADO_NORMAL y @ref FASE_INICIO.
+      */
+    Maquina_de_estados_protesis ()
+    {
+        estado_actual = ESTADO_NORMAL;
+        fase_actual = FASE_INICIO;
+        error_actual = causas_ninguna;
+
+        /**
+         * @brief inciar condiciones utilizando los valores de los umbrales de la función anterior
+         */
+        actualizacion () 
+    };
+
+
+//******* CONTINUAR MIRANDO *********//
+
+
+
+    /**
+      * @brief Cambia el estado de la prótesis y reinicia la fase a @ref FASE_1.
+      * @param nuevo_estado Nuevo estado que tomará la prótesis.
+      */
+     void cambiarEstado(Estado_Protesis nuevo_estado)
+     {
+         estado_actual = nuevo_estado;
+         fase_actual = FASE_INICIO;
+     }
+ 
+     /**
+      * @brief Cambia la fase actual de la prótesis.
+      * @param nueva_fase Nueva fase que se establecerá.
+      */
+     void cambiarFase(Fase_Estado nueva_fase)
+     {
+         fase_actual = nueva_fase;
+     }
+    
+     /**
+      * @brief Cambia a estado de seguridad al detectar cualquier anomalía. Se activa autómaticamnete o ¿manualmente por el usuario?
+      */
+    void manejoSeguridad (bool condiciones_emergencia, Estado_Protesis, Fase_Estado) 
+    {
+        estado_actual = ESTADO_SEGURIDAD; 
+
+        switch (fase_actual) 
+        {
+            case FASE_CIERRE:
+            /**
+             * @brief en el caso de que la prótesis tenga la mano cerrada, bloqueada, la abre
+             */
+                if ()
+        };
+    };
+};
+
+
